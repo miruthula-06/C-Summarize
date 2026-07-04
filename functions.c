3d6421b3ae141a5fa1623a3a105b1dc94c7828a4
Miruthula <miruthuladeventhiran@gmail.com>
2026-07-04 11:40:25 +0530

    functions

---
+++ functions.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int add(int a,int b){
+    return a+b;
+}
+void main(){
+    int c,d;
+    printf("Enter a and b value: ");
+    scanf("%d %d",&c,&d);
+    printf("%d",add(c,d));
+}git 
