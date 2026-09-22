e4b061487566db01d8158453c83af5965e48ee5a
miruthula <miruthuladeventhiran@gmail.com>
2026-09-22 21:08:20 +0530

    new file

---
+++ increment.c
@@ -0,0 +1,13 @@
+// #include<stdio.h>
+// int main(){
+//     int i=0;
+//     printf("%d %d %d %d",i,i++,++i,i);
+// }
+#include <stdio.h>
+int main() {
+    int i = 5;
+    i++;
+    ++i;
+    i++;
+    printf("%d", i);
+}
